34b9162dd314e13ed6c0fc47b90aa10c586785ec
Muhammad Naufal Ammar <36336568+ammarun11@users.noreply.github.com>
2023-01-07 22:11:48 +0700

    gptj question and answering

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
 import gradio as gr
 
-title = "GPT-J-6B"
+title = "GPT-J-question-answering"
 
 my_examples = [
-    ["The tower is 324 metres (1,063 ft) tall,"],
+    ["The Amazon rainforest (Portuguese: Floresta Amazônica or Amazônia; Spanish: Selva Amazónica, Amazonía or usually Amazonia; French: Forêt amazonienne; Dutch: Amazoneregenwoud), also known in English as Amazonia or the Amazon Jungle, is a moist broadleaf forest that covers most of the Amazon basin of South America. This basin encompasses 7,000,000 square kilometres (2,700,000 sq mi), of which 5,500,000 square kilometres (2,100,000 sq mi) are covered by the rainforest. This region includes territory belonging to nine nations. The majority of the forest is contained within Brazil, with 60% of the rainforest, followed by Peru with 13%, Colombia with 10%, and with minor amounts in Venezuela, Ecuador, Bolivia, Guyana, Suriname and French Guiana. States or departments in four nations contain Amazons in their names. The Amazon represents over half of the planet's remaining rainforests, and comprises the largest and most biodiverse tract of tropical rainforest in the world, with an estimated 390 billion individual trees divided into 16,000 species."],
     ["The Moon's orbit around Earth has"],
     ["The smooth Borealis basin in the Northern Hemisphere covers 40%"],
 ]
 
 demo = gr.Interface.load(
-    "huggingface/EleutherAI/gpt-j-6B",
+    "huggingface/ydshieh/tiny-random-gptj-for-question-answering",
     inputs=gr.Textbox(lines=5, max_lines=6, label="Input Text"),
-    title="Ammar Test GPT-J-6B",
+    title="Ammar Test GPT-J-question-answering",
     examples = my_examples,
 )
 
-demo.launch(server_name="0.0.0.0", server_port=7400)
+demo.launch(server_name="0.0.0.0", server_port=7500)
 

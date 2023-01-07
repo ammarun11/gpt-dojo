ff5ab168a977e1bc69d6e640d9d0149db0ba46c4
Muhammad Naufal Ammar <36336568+ammarun11@users.noreply.github.com>
2023-01-07 20:51:23 +0700

    sample gpt-j-6b

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import gradio as gr
 
 title = "GPT-J-6B"
 
-examples = [
+my_examples = [
     ["The tower is 324 metres (1,063 ft) tall,"],
     ["The Moon's orbit around Earth has"],
     ["The smooth Borealis basin in the Northern Hemisphere covers 40%"],
@@ -11,10 +11,9 @@ examples = [
 demo = gr.Interface.load(
     "huggingface/EleutherAI/gpt-j-6B",
     inputs=gr.Textbox(lines=5, max_lines=6, label="Input Text"),
-    title=title,
-    examples=examples,
+    title="Ammar Test GPT-J-6B",
+    examples = my_examples,
 )
-
 demo.launch(server_name="0.0.0.0", server_port=7400)
 
 if __name__ == "__main__":
